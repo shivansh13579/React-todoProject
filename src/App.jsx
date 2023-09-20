89166d06cf097f732868951066c14e0c3637ac1b
shivansh13579 <shivansh0975@gmail.com>
2023-09-20 20:54:45 +0530

    context api uses

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ function App() {
   const [todos,setTodos] = useState([])
 
   const addTodo = (todo)=>{
+     setTodos((prev)=>[{id: Date.now(),...todo},...prev])
+  }
 
+  const updateTodo = (id,todo) =>{
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) =>{
+    setTodos((prev) => prev.filter((todo)=>todo.id!==id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo )))
   }
 
   return (
